27980083ce81e16e8cf273e0a2bac413fa03515e
Jon Culver <jonathanhculver@gmail.com>
2015-02-18 20:10:07 -0500

    replace local function with prototype inhheritance

--- class3/app/widgetcurrency.js
+++ class3/app/widgetcurrency.js
@@ -1,4 +1,12 @@
-var WidgetCurrency = function(){
-	this.base = "foo";
-	this.amount = 0;
+var WidgetCurrency = function(base, amount){
+	this.base = base || "foo";
+	this.amount = amount || 0;
+
+	/*this.display = function() {
+		return this.base+" : "+ this.amount;
+	};*/
 };
+
+WidgetCurrency.prototype.display = function() {
+	return this.base+" : "+ this.amount;
+}
--- class3/test/widget_currency_spec.js
+++ class3/test/widget_currency_spec.js
@@ -5,13 +5,38 @@ describe("widgetCurrency", function() {
 });
 
 describe("defaults", function() {
+	var currency;
+
+	beforeEach(function() {
+		currency = new WidgetCurrency();
+	});
+
 	it("base is foo", function() {
-		var currency = new WidgetCurrency();
 		expect(currency.base).toEqual("foo");
 	});
+
 	it("amount is 0", function() {
-		var currency = new WidgetCurrency();
 		expect(currency.amount).toEqual(0);
+	});
+});
+
+describe("setting values with constructor", function() {
+	it("we can set base to bar", function() {
+		var currency = new WidgetCurrency("bar");
+		expect(currency.base).toEqual("bar");
+	});
+	it("we can set amount to 18", function() {
+		var currency = new WidgetCurrency(false, 18);
+		expect(currency.amount).toEqual(18);
+	});
+});
+
+describe("display", function() {
+	describe("when base is buzz and amount 10", function() {
+		it("is 'buzz : 10'", function() {
+			var currency = new WidgetCurrency("buzz", 10);
+			expect(currency.display()).toEqual('buzz : 10');
+		});
 	});
 });
 
