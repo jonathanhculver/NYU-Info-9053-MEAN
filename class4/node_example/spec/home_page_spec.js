82405d1834c1c62260e5d0f074b00bb907b75a58
Jon Culver <jonathanhculver@gmail.com>
2015-02-25 21:15:18 -0500

    adding cheerio for dom testing

--- class4/node_example/spec/home_page_spec.js
+++ class4/node_example/spec/home_page_spec.js
@@ -1,17 +1,23 @@
 var request = require("request");
+var cheerio = require("cheerio");
 
 describe("home page", function() {
 	describe("/3", function() {
 		var html;
+		var $;
 		beforeEach(function(done) {
 			request("http://localhost:8080/3", function(error, response, body) {
 				html = body;
+				$ = cheerio.load(body);
 				done(); //tells tests to wait till its done to run
 			});
 		});
 
 		it("it should contain hello", function(){
 			expect(html).toContain('hello');
+		});
+		it("it should contain 5 li items", function(){
+			expect($("li").length).toEqual(5);
 		});
 	})
 });
